708d086a644140af7cbe89510af4a579c45a6413
Leonardo Quevedo <pac.leo@hotmail.com>
2014-12-12 21:29:25 -0200

    refactor (module-placeholders) Addition of sidemenu options placeholder
    Adding the regular expressions that are going to identify where the sidemenu options should be pasted by kami.

--- js/controllers.js
+++ js/controllers.js
@@ -4,11 +4,7 @@
 
      //Placeholder for login module, if it wasn't required on generation please ignore this
 
-    $scope.menuOptions = [
-    {"title":"TAPPPPPP","uri":"/projetos", "initialHTML": "Este é um conteúdo de testes 1"},
-    {"title":"Setores","uri":"/setores","initialHTML": "Este é um conteúdo de testes 2"},
-    {"title":"Pesquisar","uri":"/search","initialHTML": "Este é um conteúdo de testes 3"}
-    ]
+    //Placeholder for kamiSidemenuOptions
   })
 
   .controller('ExampleCtrl', function($scope, $http) {
